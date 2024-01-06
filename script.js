51fa98f72dfeadd7169e970b775d6a92338ec2c6
gdsidhu <102585578+gdsidhu@users.noreply.github.com>
2024-01-06 15:42:30 +0530

    bla bla bla

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ hamburger.addEventListener("click", ()=>{
     menu.classList.toggle("slide");
 
 })
+
+const tl = gsap.timeline();
+
+tl.from("nav", {
+    translateY: "-150px",
+    duration:1
+})
+tl.from("h1", {
+    translateX: "-800px",
+})
+
+tl.from("h2", {
+    translateX: "-700px",
+})
+
+tl.from("button", {
+    translateX: "-600px",
+
+})
+
