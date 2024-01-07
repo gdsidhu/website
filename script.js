8e370880fb05ccf1ce80158985673e0a00ee3b8f
gdsidhu <102585578+gdsidhu@users.noreply.github.com>
2024-01-07 13:20:36 +0530

    added project page

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const hamburger = document.querySelector(".lines_container")
 const menu = document.querySelector("#slider")
+const scroll = document.querySelector(".scroll_down")
+console.log(scroll)
+
+
 hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("active");
     menu.classList.toggle("slide");
@@ -12,16 +16,27 @@ tl.from("nav", {
     translateY: "-150px",
     duration:1
 })
-tl.from("h1", {
-    translateX: "-800px",
-})
+// tl.from("h1", {
+//     translateX: "-800px",
+// })
 
-tl.from("h2", {
-    translateX: "-700px",
-})
+// tl.from("h2", {
+//     translateX: "-700px",
+// })
 
 tl.from("button", {
     translateX: "-600px",
 
 })
+tl.to(scroll, {
+    translateY:"-20px",
+    repeat:4,
+    delay:3,
+    yoyo:true,
+})
+tl.to(scroll, {
+opacity:0,
+})
+
+
 
